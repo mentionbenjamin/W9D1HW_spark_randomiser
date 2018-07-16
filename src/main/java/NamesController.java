5a8e8436fce60bebe1849f23a9911f6556444d17
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-16 16:22:53 +0100

    set up arraylist in names controller

--- src/main/java/NamesController.java
+++ src/main/java/NamesController.java
@@ -1,2 +1,22 @@
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+
+import static spark.Spark.staticFileLocation;
+
 public class NamesController {
+
+    public static void main(String[] args) {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+        staticFileLocation("/public");
+
+        ArrayList<Name> names = new ArrayList<>();
+        Name name1 = new Name("John", 24);
+        Name name2 = new Name("Joe", 75);
+        Name name3 = new Name("Jill", 43);
+        Name name4 = new Name("James", 25);
+        Name name5 = new Name("Jack", 86);
+        Name name6 = new Name("Jo-Anne", 45);
+    }
+
 }
