b221faff1fef33cc6193de5d8aceb2e423236c32
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-17 06:53:29 +0100

    shuffled around files. now can get one random name

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,26 +1,30 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Name {
-
-
 
     private ArrayList<String> names;
 
     public Name() {
         this.names = new ArrayList<String>();
-        names.add("Joe");
-        names.add("Jack");
-        names.add("Jill");
-        names.add("Jo-Anne");
-        names.add("Jabba");
+        names.add("Jim");
+        names.add("John");
+        names.add("Jacob");
+        names.add("Jemima");
+        names.add("Jemma");
+
     }
 
-    public String getRandomName(){
+    public String getRandomName() {
         Collections.shuffle(names);
         return names.get(0);
     }
 
-
+    public ArrayList<String> getRandom2Names() {
+        ArrayList<String> twoNames = new ArrayList<>();
+        Collections.shuffle(names);
+        twoNames.add(names.get(0));
+        twoNames.add(names.get(1));
+        return twoNames;
+    }
 }
--- src/main/java/NamesController.java
+++ src/main/java/NamesController.java
@@ -2,38 +2,25 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class NamesController {
-
     public static void main(String[] args) {
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
         staticFileLocation("/public");
 
-        ArrayList<Name> names = new ArrayList<>();
-        Name name1 = new Name("John",    24);
-        Name name2 = new Name("Joe",     75);
-        Name name3 = new Name("Jill",    43);
-        Name name4 = new Name("James",   25);
-        Name name5 = new Name("Jack",    86);
-        Name name6 = new Name("Jo-Anne", 45);
 
-        names.add(name1);
-        names.add(name2);
-        names.add(name3);
-        names.add(name4);
-        names.add(name5);
-        names.add(name6);
+        get("/one", (req, res) -> {
+            Name name = new Name();
+            String foundName = name.getRandomName();
 
-        get("names", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
-            model.put("names", names);
-            model.put("template", "names.vtl");
-            return new ModelAndView(model, "layout.vtl");
+            model.put("names", foundName);
+            return new ModelAndView(model, "one.vtl");
         }, velocityTemplateEngine);
     }
-
 }
